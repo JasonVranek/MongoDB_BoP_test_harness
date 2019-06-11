c2dde80d0066932a5809a4df64bca44046eb0d33
Jason V <jvranek@ucsc.edu>
2019-06-10 17:54:39 -0700

    modified back start_mongo to open in separate terminals

--- start_mongo.sh
+++ start_mongo.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# x-terminal-emulator -e sudo mongod --port 27017 --dbpath /var/lib/mongodb --replSet rs0 &
-# x-terminal-emulator -e sudo mongod --port 27018 --dbpath /var/lib/mongodb1 --replSet rs0 &
-# x-terminal-emulator -e sudo mongod --port 27019 --dbpath /var/lib/mongodb2 --replSet rs0 &
+x-terminal-emulator -e sudo mongod --port 27017 --dbpath /var/lib/mongodb --replSet rs0 &
+x-terminal-emulator -e sudo mongod --port 27018 --dbpath /var/lib/mongodb1 --replSet rs0 &
+x-terminal-emulator -e sudo mongod --port 27019 --dbpath /var/lib/mongodb2 --replSet rs0 &
 
-mongod --port 27017 --dbpath /var/lib/mongodb --replSet rs0 &
-mongod --port 27018 --dbpath /var/lib/mongodb1 --replSet rs0 &
-mongod --port 27019 --dbpath /var/lib/mongodb2 --replSet rs0 &
+# If x-terminal-emulator is unavialable, run the following commands in their own shells:
+# sudo mongod --port 27017 --dbpath /var/lib/mongodb --replSet rs0 
+# sudo mongod --port 27018 --dbpath /var/lib/mongodb1 --replSet rs0 
+# sudo mongod --port 27019 --dbpath /var/lib/mongodb2 --replSet rs0 &
 
 exec $SHELL
